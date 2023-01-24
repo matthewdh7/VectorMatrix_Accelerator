84e1aa31c703cc30fd351e687a65f106324411b8
Huwan Peng <hwpeng20@gmail.com>
2023-01-23 18:36:10 -0800

    Update constraints.tcl

--- pow2/cfg/constraints.tcl
+++ pow2/cfg/constraints.tcl
@@ -9,10 +9,10 @@ create_clock -name clk -period 10.0 [get_ports clk_i]
 set_clock_uncertainty 0.100 [get_clocks clk]
 
 # Always set the input/output delay as half periods for clock setup checks
-set_input_delay  5.0 -setup -clock [get_clocks clk] [all_inputs]
-set_output_delay 5.0 -setup -clock [get_clocks clk] [all_outputs]
+set_input_delay  5.0 -max -clock [get_clocks clk] [all_inputs]
+set_output_delay 5.0 -max -clock [get_clocks clk] [remove_from_collection [all_outputs] [get_ports clk_o]]
 
 # Always set the input/output delay as 0 for clock hold checks
-set_output_delay 0.0 -hold  -clock [get_clocks clk] [all_outputs] 
-set_input_delay  0.0 -hold  -clock [get_clocks clk] [all_inputs]
+set_input_delay  0.0 -min -clock [get_clocks clk] [all_inputs]
+set_output_delay 0.0 -min -clock [get_clocks clk] [remove_from_collection [all_outputs] [get_ports clk_o]]
 
