cf1e9bb76904e7b3c54f8ffef1e8c8f7f58c42e2
Matthew Hung <mhung20@linuxsrv01.ece.uw.edu>
2024-05-07 12:05:38 -0700

    remove pow2, progress on lane

--- vector/v/lane.sv
+++ vector/v/lane.sv
@@ -24,7 +24,7 @@ module lane #(parameter els_p = 32  // number of vectors stored
     , input  logic start_i
 
     , input  logic [vdw_p-1:0] scalar_i
-
+    , input  logic [vdw_p-1:0] w_data_i
     , output logic [vdw_p-1:0] r_data_o
     , output logic v_o
 
@@ -40,17 +40,7 @@ module lane #(parameter els_p = 32  // number of vectors stored
 
     enum {s_IDLE, s_LOOP, s_DONE} ps, ns;
 
-    // // datapath
-    // logic [vdw_p-1:0] scalar_li, , alu_result_lo; 
-    // logic [op_width_p-1:0] alu_op_li;
-    // logic [counter_width_lp-1:0] count_lo;
-
-    // logic [local_addr_width_lp-1:0] r_addr_lo;
-
-    // // control
-    // logic counter_en_li, init;
-
-    // next state logic
+    //// next state logic
     always_comb begin
         case (ps)
             s_IDLE: ns = start ? s_LOOP : s_IDLE;
@@ -64,28 +54,72 @@ module lane #(parameter els_p = 32  // number of vectors stored
         else        ps <= ns;
     end
 
+    //// control logic (besides w_en)
+    logic counter_set_i, counter_en_i;
+    logic 
+    assign counter_set_i = (ps == s_IDLE) & (ns == s_LOOP);
+    assign counter_en_i = (ps == s_LOOP);
+
+    //// datapath 
+
     // REG/DECODE stage
-    // should be all external inputs
+    logic [vdw_p-1:0] REG_addr;
+    logic REG_w_data_ext, REG_w_en;
+
+    assign REG_addr = my_id_i + (lanes_p * counter_lo);
+    assign REG_w_data_ext = w_data_i;
+    assign REG_w_en = //TODO
 
     // EX stage
-    logic [vdw_p-1:0] alu_a_li, alu_b_li, alu_result_lo;
+    logic [vdw_p-1:0] alu_a_li, alu_b_li, alu_result_lo, EX_r1_data, EX_scalar;
     logic [op_width_p-1:0] alu_op_li;
     logic [local_addr_width_lp-1:0] EX_w_addr;
     logic [vdw_p-1:0] EX_w_data_ext; // external write data, if opcode is a write
     logic EX_w_en;
 
     always_ff @(posedge clk_i) begin
-        alu_a_li <= 
-        alu_b_li <=
-        EX_w_addr <=
-        EX_w_data_ext <= 
-        EX_w_en <= 
+        alu_a_li <= r0_data_i;
+        // alu_b_li handled by mux
+        EX_w_addr <= REG_addr;
+        EX_w_data_ext <= REG_w_data_ext;
+        EX_w_en <= REG_w_en;
+        EX_scalar <= scalar_i;
+        EX_r1_data <= r1_data_i;
     end
+
+    bsg_mux    #(.width_p(vdw_p)
+                ,.els_p(2))
+        alu_b_mux
+            (.data_i    ({EX_scalar, EX_r1_data})
+            ,.sel_i     ()//TODO
+            ,.data_o    (alu_b_li)
+            );
 
     // WB stage
     logic [local_addr_width_lp-1:0] WB_w_addr;
     logic [vdw_p-1:0] WB_w_data_ext, WB_w_data_alu, WB_w_data;
     logic WB_w_en;
+
+    always_ff @(posedge clk_i) begin
+        WB_w_addr <= EX_w_addr;
+        WB_w_data_ext <= EX_w_data_ext;
+        WB_w_data_alu <= alu_result_lo;
+        WB_w_en <= EX_w_en;
+    end
+
+    bsg_mux    #(.width_p(vdw_p)
+                ,.els_p(2))
+        w_data_mux
+            (.data_i    ({WB_w_data_ext, WB_w_data_alu})
+            ,.sel_i     ()//TODO
+            ,.data_o    (WB_w_data)
+            );
+
+    // WB external outputs
+    assign w_addr_o = WB_w_addr;
+    assign w_data_o = WB_w_data;
+    assign w_en_o = WB_w_en;
+
 
     bsg_counter_set_en #(.max_val_p(counter_max_lp))
         addr_counter
